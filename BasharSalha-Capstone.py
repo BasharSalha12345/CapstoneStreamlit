bf055c85b3bec717edd8a6aaf84d51c62a4096bc
BasharSalha12345 <132677923+BasharSalha12345@users.noreply.github.com>
2023-05-07 10:06:53 +0300

    Update BasharSalha-Capstone.py

--- BasharSalha-Capstone.py
+++ BasharSalha-Capstone.py
@@ -16,12 +16,6 @@ with st.expander('Analyze Text'):
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
 
 
-with st.expander('Analyze CSV'):
-    upl = st.file_uploader('Upload file')
-
-    def score(x):
-        blob1 = TextBlob(x)
-        return blob1.sentiment.polarity
 
 #
     def analyze(x):
@@ -31,25 +25,3 @@ with st.expander('Analyze CSV'):
             return 'Negative'
         else:
             return 'Neutral'
-
-#
-    if upl:
-        df = pd.read_excel(upl)
-        del df['Unnamed: 0']
-        df['score'] = df['body'].apply(score)
-        df['analysis'] = df['score'].apply(analyze)
-        st.write(df.head(10))
-
-        @st.cache
-        def convert_df(df):
-            # IMPORTANT: Cache the conversion to prevent computation on every rerun
-            return df.to_csv().encode('utf-8')
-
-        csv = convert_df(df)
-
-        st.download_button(
-            label="Download data as CSV",
-            data=csv,
-            file_name='sentiment.csv',
-            mime='text/csv',
-        )
